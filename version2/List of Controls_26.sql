c4d03d38582f8614856a1dc1008c1a7e9bed9067
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2022-08-24 08:28:30 +0300

    Last change

--- version2/List of Controls_26.sql
+++ version2/List of Controls_26.sql
@@ -42,7 +42,7 @@ SELECT
         
          WHERE
          1=1
-         AND A.FORM  = 29
+         AND A.FORM  = 26
          
          AND A.CONTROL_VERS <=:pPERIOADA 
                
@@ -57,7 +57,7 @@ SELECT
         
          WHERE
          1=1
-         AND B.FORM  = 29
+         AND B.FORM  = 26
          AND B.STATUT <> '3'
          
         
