bcf14285eb10a5c6e92d235e6c7e8a73fec1af87
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2022-09-10 07:51:22 +0300

    Fixed some bugs

--- version2/21_VANZ_137.js
+++ version2/21_VANZ_137.js
@@ -24,40 +24,39 @@
 
 webform.validators.vanz21 = function (v, allowOverpass) {
     var values = Drupal.settings.mywebform.values;
+    
+    function fun_row_26_024(row) {
+        var i;
+        i = row;
+        if (
 
-    function validate_autofields(item) {
-        var values = Drupal.settings.mywebform.values;
+            10 == i || 11 == i || 12 == i || 13 == i || 14 == i || 15 == i || 16 == i
+            || 17 == i || 18 == i || 19 == i || 20 == i || 21 == i
+            || 22 == i || 23 == i || 24 == i || 25 == i
+            || 26 == i || 27 == i || 28 == i || 29 == i || 30 == i || 31 == i || 32 == i || 33 == i || 34 == i
+            || 35 == i || 36 == i || 37 == i || 38 == i || 39 == i || 40 == i || 41 == i || 42 == i || 43 == i
+            || 44 == i || 45 == i || 46 == i || 47 == i || 48 == i || 49 == i || 50 == i || 51 == i || 52 == i
+            || 53 == i || 54 == i || 55 == i || 56 == i || 57 == i || 58 == i || 59 == i || 60 == i || 61 == i || 62 == i
+            || 63 == i || 64 == i || 65 == i || 66 == i || 67 == i || 68 == i || 69 == i || 70 == i || 71 == i
+            || 72 == i || 73 == i || 74 == i || 75 == i || 76 == i || 77 == i || 78 == i || 79 == i || 80 == i
+            || 81 == i || 82 == i || 83 == i || 84 == i || 85 == i || 86 == i || 87 == i || 88 == i || 89 == i
+            || 90 == i || 91 == i || 92 == i || 93 == i || 94 == i || 95 == i || 96 == i || 97 == i || 98 == i
+            || 99 == i || 100 == i || 101 == i || 102 == i || 103 == i || 104 == i || 105 == i || 106 == i || 107 == i
+            || 108 == i || 109 == i || 110 == i || 135 == i || 134 == i || 133 == i || 132 == i || 131 == i || 130 == i
+            || 136 == i
 
-        var field = Drupal.settings.mywebform.fields[item.rezField];
-        var decimal_length = 0;
-        switch (field.type) {
-            case 'float':
-                decimal_length = field.decimal_length;
-                break
-            case 'money':
-                decimal_length = 2;
-                break;
-        }
 
-        if (formatNumber(item.callback(), decimal_length) != formatNumber(values[item.rezField], decimal_length)) {
-            var msg = item.text;
-            if (typeof msg == 'function') {
-                msg = msg();
-            }
 
-            webform.errors.push({
-                'fieldName': item.rezField,
-                'index': 0,
-                'weight': parseInt(item.err),
-                'msg': concatMessage('67-' + item.err, '', msg)
-            });
-        }
+
+        )
+            return true;
+
     }
-
+ 
     // Start 26-002
     for (var i = 10; i <= 136; i++) {
         {
-
+            if (fun_row_26_024(i)) {
             if (!isNaN(Number(values["CAP1_R" + i + "_C1"])) 
                 && !isNaN(Number(values["CAP1_R" + i + "_C2"]))
                 && !isNaN(Number(values["CAP1_R" + i + "_C3"]))
@@ -70,11 +69,22 @@ webform.validators.vanz21 = function (v, allowOverpass) {
            var col2 = Number(values["CAP1_R" + i + "_C2"]);
            var col3 = toFloat(values["CAP1_R" + i + "_C3"]).toPrecision(3);
 
-         
+                function notZero(n) {
+                    n = +n;  // Coerce to number.
+                    if (!n) {  // Matches +0, -0, NaN
+                        throw new Error('Invalid dividend ' + n);
+                    }
+                    return n;
+                }
           
-           var col2DevCol1 = Math.round((Number(values["CAP1_R" + i + "_C2"]) / Number(values["CAP1_R" + i + "_C1"])) * 100) / 100;
+                if (col1 > 0 && col2 > 0){
+                
+                var col2DevCol1 = Math.round((col2 / notZero(col1)) * 100) / 100; }
 
-                if (col2DevCol1 != col3  && col1 != 0 && col2 != 0 ) {
+
+
+
+                if (col2DevCol1 != col3  && col1 > 0 && col2 > 0 ) {
                 webform.errors.push({
                     'fieldName': 'CAP1_R' + i + '_C3',
                     'weight': 9,
@@ -83,7 +93,7 @@ webform.validators.vanz21 = function (v, allowOverpass) {
             }
             }
         }
-
+    }
     }
 
  //End 26-002
@@ -93,7 +103,7 @@ webform.validators.vanz21 = function (v, allowOverpass) {
     // Start 26-003
     for (var i = 10; i <= 136; i++) {
         {
-
+            if (fun_row_26_024(i)) {
             if (!isNaN(Number(values["CAP1_R" + i + "_C4"])) && !isNaN(Number(values["CAP1_R" + i + "_C5"]))
                 && !isNaN(Number(values["CAP1_R" + i + "_C6"]))
                 && !isNaN((Number(values["CAP1_R" + i + "_C5"]) / Number(values["CAP1_R" + i + "_C4"])))
@@ -106,7 +116,21 @@ webform.validators.vanz21 = function (v, allowOverpass) {
                 var col6 = toFloat(values["CAP1_R" + i + "_C6"]).toPrecision(3);
 
 
-                var col5DevCol4 = Math.round((Number(values["CAP1_R" + i + "_C5"]) / Number(values["CAP1_R" + i + "_C4"])) * 100) / 100;
+            
+                function notZero(n) {
+                    n = +n;  // Coerce to number.
+                    if (!n) {  // Matches +0, -0, NaN
+                        throw new Error('Invalid dividend ' + n);
+                    }
+                    return n;
+                }
+
+                if (col4 > 0 && col5 > 0) {
+
+                    var col5DevCol4 = Math.round((col5 / notZero(col4)) * 100) / 100;
+                }
+
+
 
                 if (col5DevCol4 != col6 && col4 != 0 && col5 != 0) {
                     webform.errors.push({
@@ -117,7 +141,7 @@ webform.validators.vanz21 = function (v, allowOverpass) {
                 }
             }
         }
-
+      }
     }
 
  //End 26-003
@@ -170,34 +194,8 @@ webform.validators.vanz21 = function (v, allowOverpass) {
 
 
 
-    // Start 26-024
-    function fun_row_26_024(row) {
-        var i;
-        i = row;
-        if (
-
-            10 == i || 11 == i || 12 == i || 13 == i || 14 == i || 15 == i || 16 == i
-            || 17 == i || 18 == i || 19 == i || 20 == i || 21 == i
-            || 22 == i || 23 == i || 24 == i || 25 == i
-            || 26 == i || 27 == i || 28 == i || 29 == i || 30 == i || 31 == i || 32 == i || 33 == i || 34 == i
-            || 35 == i || 36 == i || 37 == i || 38 == i || 39 == i || 40 == i || 41 == i || 42 == i || 43 == i
-            || 44 == i || 45 == i || 46 == i || 47 == i || 48 == i || 49 == i || 50 == i || 51 == i || 52 == i
-            || 53 == i || 54 == i || 55 == i || 56 == i || 57 == i || 58 == i || 59 == i || 60 == i || 61 == i || 62 == i
-            || 63 == i || 64 == i || 65 == i || 66 == i || 67 == i || 68 == i || 69 == i || 70 == i || 71 == i
-            || 72 == i || 73 == i || 74 == i || 75 == i || 76 == i || 77 == i || 78 == i || 79 == i || 80 == i
-            || 81 == i || 82 == i || 83 == i || 84 == i || 85 == i || 86 == i || 87 == i || 88 == i || 89 == i
-            || 90 == i || 91 == i || 92 == i || 93 == i || 94 == i || 95 == i || 96 == i || 97 == i || 98 == i
-            || 99 == i || 100 == i || 101 == i || 102 == i || 103 == i || 104 == i || 105 == i || 106 == i || 107 == i
-            || 108 == i || 109 == i || 110 == i || 135 == i || 134 == i || 133 == i || 132 == i || 131 == i || 130 == i
-            || 136 == i
-
-
-
-
-        )
-            return true;
-
-    }
+  //  Start 26-024
+ 
 
     for (var i = 10; i <= 136; i++) {
         {
@@ -225,7 +223,7 @@ webform.validators.vanz21 = function (v, allowOverpass) {
 
     
 
-    // Start 26-004
+   // Start 26-004
     for (var i = 10; i <= 136; i++) {
         {
             if (fun_row_26_024(i)) {
@@ -260,7 +258,7 @@ webform.validators.vanz21 = function (v, allowOverpass) {
 
  //End 26-004
 
-    // Start 26-005
+  //  Start 26-005
     for (var i = 10; i <= 136; i++) {
         {
             if (fun_row_26_024(i)) {
@@ -365,31 +363,31 @@ webform.validators.vanz21 = function (v, allowOverpass) {
 // End 26-023
 // Start 26 - 007
 
-    for (var i = 1; i <= 5; i++) {
+    // for (var i = 1; i <= 5; i++) {
         
-        if (i =! 3) {
-        if (!isNaN(Number(values["CAP1_R46_C" + i]))){
-        var R46_C  = Number(values["CAP1_R46_C" + i]);}
+    //     if (i =! 3) {
+    //     if (!isNaN(Number(values["CAP1_R46_C" + i]))){
+    //     var R46_C  = Number(values["CAP1_R46_C" + i]);}
 
 
-        if (!isNaN(Decimal(values["CAP1_R47_C" + i] || 0)
-            .plus(values["CAP1_R48_C" + i] || 0)))
-         {   
-        var R47_48 = Decimal(values["CAP1_R47_C" + i] || 0)
-            .plus(values["CAP1_R48_C" + i] || 0);}
+    //     if (!isNaN(Decimal(values["CAP1_R47_C" + i] || 0)
+    //         .plus(values["CAP1_R48_C" + i] || 0)))
+    //      {   
+    //     var R47_48 = Decimal(values["CAP1_R47_C" + i] || 0)
+    //         .plus(values["CAP1_R48_C" + i] || 0);}
 
 
-        if (R46_C < R47_48) {
-            webform.errors.push({
-                'fieldName': 'CAP1_R46_C' + i,
-                'weight': 7,
-                'msg': Drupal.t('Cod eroare: 26-007. Cap.I, Rind. 46 >= Rind.47 + Rind.48 - @R46_C < @R47_48', { '@R46_C': R46_C, '@R47_48': R47_48, })
-            });
-        }
-    }
+    //     if (R46_C < R47_48) {
+    //         webform.errors.push({
+    //             'fieldName': 'CAP1_R46_C' + i,
+    //             'weight': 7,
+    //             'msg': Drupal.t('Cod eroare: 26-007. Cap.I, Rind. 46 >= Rind.47 + Rind.48 - @R46_C < @R47_48', { '@R46_C': R46_C, '@R47_48': R47_48, })
+    //         });
+    //     }
+    // }
 
 
-    }
+    // }
 
     // End 26 - 007
 
